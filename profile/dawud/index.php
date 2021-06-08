3088162296275c9164ea4373961c78e1809ef726
1305rabea <1305rabea@gmail.com>
2021-06-08 15:54:35 +0200

    profile folder added

---
+++ profile/dawud/index.php
@@ -0,0 +1,133 @@
+<html style="margin: 0px; padding: 0px;">
+<head>
+		<link rel="icon" 
+		      type="image/ico"
+		      href="images/favicon.ico"
+		/>
+	        <style>
+			.border {
+				border: 2px black solid;
+			}
+			.a{
+				text-decoration: none;
+			}
+		</style>
+
+		<meta charset="utf-8" />
+		<title>Dawud Resume</title>
+		<meta name="description" content="app, web app, responsive, admin dashboard, admin, flat, flat ui, ui kit, off screen nav" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
+
+		<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+		<link rel="stylesheet" href="css/animate.css" type="text/css" />
+		<link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" />
+		<link rel="stylesheet" href="css/font.css" type="text/css" />
+		<link rel="stylesheet" href="js/fuelux/fuelux.css" type="text/css" />
+		<link rel="stylesheet" href="css/app.css" type="text/css" />
+		<!--[if lt IE 9]>
+		<script src="js/ie/html5shiv.js"></script>
+		<script src="js/ie/respond.min.js"></script>
+		<script src="js/ie/excanvas.js"></script>
+		<![endif]-->
+
+</head>
+
+<body style="margin: 0px; padding: 0px;">
+	<div style="overflow: hidden; width:100%">
+		<div style="float: left; margin: 20px;">
+			<img src="http://192.73.236.32/profile/dawud/images/bhai.png" width="100" />
+			<h2 align="left">Mohammad Dawud Ansari</h2>
+			<div style="float: none;">
+				Furtwanger Strasse 6,
+				<br/>
+				Boeblingen - 71034
+				<br/>
+				Germany
+				<br/>
+				Email: MohammadDawud{dot}Ansari{at}de{dot}bosch{dot}com
+				<br/>
+				<br/>
+				<strong><a href="http://192.73.236.32/profile/dawud/resume" style="text-decoration: underline;" target="_blank">Full CV</a></strong>
+				<br/>
+				<h3>Publications</h3>
+				 <ul>
+				  <li>
+						<span>
+						<strong>Mohammad Dawud Ansari</strong>, Stephan Krauß, Oliver Wasenmüller, and <br>Didier Stricker. 
+						"ScaleNet: Scale Invariant Network for Semantic <br>Segmentation in Urban DrivingScenes." 
+						<i>International Conference on <br>Computer Vision Theory and Applications , Funchal, Madeira - Portugal;</i> <br>January 2018.
+						</span>
+						<strong><a href="https://www.scitepress.org/Papers/2018/67230/67230.pdf" style="text-decoration: underline;" target="_blank">paper</a></strong>
+				  </li>
+				  <li>
+						<span>
+						<strong>Mohammad Dawud Ansari</strong>, Vladislav Golyanik, and Didier Stricker. 
+						<br>"Scalable Dense Monocular Surface Reconstruction." 
+						<i>International Conference <br>on 3D Vision (3DV), Qingdao, China;</i> October 2017.
+						</span>
+						<strong><a href="https://ieeexplore.ieee.org/document/8374560" style="text-decoration: underline;" target="_blank">paper</a></strong>
+				  </li>					 
+				  <li>Oliver Wasenmüller , <strong>Mohammad Dawud Ansari</strong>, Didier Stricker. 				  
+				      <br>"DNA-SLAM: Dense Noise Aware SLAM for ToF RGB-D Cameras". 
+					  <br><i>Asian Conference on Computer Vision Workshop (ACCV workshop), <br>Taiwan;</i> Nov'2016
+					  <strong><a href="https://www.researchgate.net/publication/308614770_DNA-SLAM_Dense_Noise_Aware_SLAM_for_ToF_RGB-D_Cameras?ev=contentfeed&homeFeedVariantCode=d_EU&_iepl%5BviewId%5D=pDi3sOHK7iQsx1T33MdIOXbV&_iepl%5BsingleItemViewId%5D=8Es1hbWAvVCUAPVdjFMsRDaw&_iepl%5BactivityId%5D=757144903356422%2C757144261627911%2C757143825424385&_iepl%5BactivityType%5D=person_add_publication&_iepl%5BactivityTimestamp%5D=1474958612&_iepl%5BhomeFeedVariantCode%5D=d_EU&_iepl%5Bcontexts%5D%5B0%5D=homeFeed&_iepl%5BinteractionType%5D=publicationView" style="text-decoration: underline;" target="_blank">paper</a></strong>
+				  </li>
+				  <li>
+						<span>
+						Gurjashan Singh Pannu, <strong>Mohammad Dawud Ansari</strong>, and Pritha Gupta. 
+						<br>"Design and Implementation of Autonomous Car using Raspberry Pi." 
+						<br> <i>International Journal of Computer Applications (IJCA)</i> 113, no. 9 (2015).
+						</span>
+						<strong><a href="http://research.ijcaonline.org/volume113/number9/pxc3901789.pdf" style="text-decoration: underline;" target="_blank">paper</a></strong>
+				  </li>						  
+				</ul> 			
+			</div>						
+		</div>
+
+		<div style="float: left; margin:20px 20px 20px 0">
+			<H3 style="padding: 10px; margin:0px;">PROJECT: Autonomous Car</H1>
+			<div>
+				<iframe width="360" height="250" src="https://www.youtube.com/embed/VcWkJFspSf4" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<br/><br/><br/>
+			<div>
+				<iframe width="360" height="250" src="https://www.youtube.com/embed/Ho48goh7wHo" frameborder="0" allowfullscreen></iframe>
+			</div>				
+		</div>
+		
+		<div style="float: left; margin:20px 20px 20px 0">
+			<H3 style="padding: 10px; margin:0px;">PROJECT: DNA-SLAM</H1>
+			<!--- <div> --->
+			<div style="margin-left: 10 px">
+				<iframe width="360" height="250" src="https://www.youtube.com/embed/wOPXOlg393E?ecver=2" frameborder="0" allowfullscreen>
+				</iframe>
+			</div>
+			
+			<H3 style="padding: 10px; margin:0px;">PROJECT: Scalable Dense NRSfM</H1>
+			<div>
+				<iframe width="360" height="250" src="https://www.youtube.com/embed/XVl6XOVZroA" frameborder="0" allowfullscreen>
+				</iframe>
+			</div>
+		</div>
+	</div>
+
+		<script src="js/jquery.min.js"></script>
+		<!-- Bootstrap -->
+		<script src="js/bootstrap.js"></script>
+		<!-- App -->
+		<script src="js/app.js"></script>
+		<script src="js/app.plugin.js"></script>
+		<script src="js/slimscroll/jquery.slimscroll.min.js"></script>
+		<!-- fuelux -->
+		<script src="js/fuelux/fuelux.js"></script>
+
+		<!-- for form wizard -->
+		<script src="js/parsley/parsley.min.js"></script>
+</body>
+</html>
